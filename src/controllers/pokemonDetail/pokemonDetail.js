7a36ba610c594a0e95afe68b1beffaa252d5eca7
nisa <nisa@local.fr>
2020-09-27 15:41:10 +0700

    add catch component, save component, add 50% probability

--- src/controllers/pokemonDetail/pokemonDetail.js
+++ src/controllers/pokemonDetail/pokemonDetail.js
@@ -1,4 +1,4 @@
-import React, {useCallback, useEffect} from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {shallowEqual, useDispatch, useSelector} from 'react-redux';
 import {useParams} from 'react-router-dom';
 import {fetchOnePokemon} from '../../redux/reduxPokemonList';
@@ -9,24 +9,32 @@ const Loading = _ => (
     </>
 )
 
-const savePokemon = () => (
+const SavePokemon = () => (
     <div className="save">
         Save Pokemon
     </div>
 )
 
+const CatchPokemon = ({lempar}) => (
+    <div className="catch">
+        <div onClick={lempar}>Catch The Pokemon</div>
+    </div>
+)
+
 const PokemonDetailContainer = ({detail}) => (
-    <div className="pokemons-detail">
+    <>
         <div className="foto">
             <img src={detail.photo} alt={detail.name} width="200px"/>
         </div>
         <div className="nama">{detail.name}</div>
-    </div>
+    </>
 )
 
 const PokemonDetail = () => {
+    const throwProbability = Math.floor(Math.random() * Math.floor(2))
     const dispatch = useDispatch(); // hook dispatch
     const {name} = useParams(); // ambil nama pokemon
+    const [isCaught, setIsCaught] = useState(false);
 
     // const data = useSelector(state => state.pokemonxDetail.data); // data pokemon
     // const isFetching = useSelector(state => state.pokemonxDetail.isFetching); // loading status
@@ -53,6 +61,7 @@ const PokemonDetail = () => {
 
     useEffect(() => {
         document.title = `Detail - Pokemon`;
+        console.log(throwProbability);
 
         // jika data tidak exist di redux maka hit fungsi boundAction()
         // jika data exist tapi nama pokemon yang direquest tidak ada di redux maka hit fungsi boundAction()
@@ -60,13 +69,19 @@ const PokemonDetail = () => {
 
     }, [loadPokemon, data, name]);
 
+    const catchUsingPokemonBall = () => {
+        console.log('catchUsingPokemonBall')
+    }
 
     return (
         <>
             {isFetching ? (
                 <Loading/>
             ) : (
-                <PokemonDetailContainer detail={data}/>
+                <div className="pokemons-detail">
+                    <PokemonDetailContainer detail={data} />
+                    <CatchPokemon throw={catchUsingPokemonBall} />
+                </div>
             )}
         </>
     )
