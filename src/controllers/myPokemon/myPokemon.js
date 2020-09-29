453c4082b7f17eb38937ad51e627b0d73ac7b36d
nisa <nisa@local.fr>
2020-09-29 18:16:02 +0700

    my pokemon page, release all callback, release one callback

--- src/controllers/myPokemon/myPokemon.js
+++ src/controllers/myPokemon/myPokemon.js
@@ -1,20 +1,71 @@
 import React, {useCallback, useEffect, useState} from 'react';
 import {useDispatch, useSelector} from 'react-redux';
+import {releaseOnePokemon, releaseAllMyPokemon} from '../../redux/reduxPokemonList';
+
+const MyPokemonRelaseAll = ({releaseAllAction}) => (
+    <>
+        <div className="alert alert-notif" onClick={releaseAllAction}>
+            Relase All Pokemons
+        </div>
+    </>
+)
+
+const MyPokemonNotification = _ => (
+    <>
+        <div className="alert alert-notif">
+            You haven't catch any pokemon yet -_-
+        </div>
+    </>
+)
+
+const MyPokemonContainer = ({data, releaseAction}) => (
+    <>
+        <h1 title="My Pokemon">My Pokemon</h1>
+        {
+            data.map((pokemon, index) => (
+                <div key={index}>
+                    {pokemon.pokemonname}
+                    {pokemon.nickname}
+                    <nav onClick={() => releaseAction(pokemon.nickname)}>
+                        Release
+                    </nav>
+                </div>
+            ))
+        }
+    </>
+)
 
 const MyPokemon = () => {
-    const dispatch = useDispatch(); // hook dispatch
-
+    const dispatch = useDispatch();
     const data = useSelector(state => state.pokemonxMy.mypokemons);
-    const dataPokemons = useSelector(state => state.pokemonx.data);
 
-    console.log('data');
-    console.log(data);
+    const releaseOnePokeonAction = useCallback((data) => {
+        return dispatch(releaseOnePokemon({ mypokemons: data }));
+    }, [dispatch]);
+
+    const releasePokemon = (nickname) => {
+        const res = data.filter(nn => nn.nickname !== nickname);
+        if(res) releaseOnePokeonAction(res);
+    }
+
+    const releaseAllPokemonAction = useCallback(() => {
+        return dispatch(releaseAllMyPokemon())
+    }, [dispatch])
+
+    const releaseAllPokemon = () => {
+        if(data) releaseAllPokemonAction();
+    }
 
     return(
         <>
-           <div className="alert alert-notif">
-               You haven't catch any pokemon yet -_-
-           </div>
+            { data.length > 0 ? (
+                <>
+                    <MyPokemonContainer data={data} releaseAction={releasePokemon} />
+                    <MyPokemonRelaseAll releaseAllAction={releaseAllPokemon} />
+                </>
+            ) : (
+                <MyPokemonNotification />
+            )}
         </>
     )
 }
